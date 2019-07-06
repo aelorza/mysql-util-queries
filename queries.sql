45917db91e8e520c15d3dffa71a75fbed7ba00a8
aelorza <34943098+aelorza@users.noreply.github.com>
2019-07-05 19:16:38 -0500

    Comentado

--- queries.sql
+++ queries.sql
@@ -1,4 +1,7 @@
+#permite actualizar los precios de una venta en curso, modificar priceX y idUser según se necesite
+
 UPDATE tempsale ts
   INNER JOIN items i
   ON ts.idItem = i.idItem
-SET ts.finalPrice = i.price12 
+SET ts.finalPrice = i.price12
+WHERE ts.idUser = 1
